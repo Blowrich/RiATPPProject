923e3c595ea205a0ede7e89650b457a01014f8a0
fominykhartur <fominykhartur+GitHub@gmail.com>
2018-10-31 18:49:16 +0300

    Add SimpleScript

---
+++ Game/Assets/Scripts/SimpleScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleScript : MonoBehaviour {
+
+    public int Temp=0;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Temp++;
+        if (Temp == 1337)
+            Temp = 0;
+	}
+}
