f45ff0bca1ea41ca1a9e6c0f23d123911688a889
fominykhartur <fominykhartur+GitHub@gmail.com>
2018-12-19 21:39:16 +0300

    World Generation

--- Game/Assets/Scripts/MapDisplay.cs
+++ Game/Assets/Scripts/MapDisplay.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
-public class MapDisplay : MonoBehaviour {
-
+public class MapDisplay : MonoBehaviour
+{
     public Renderer textureRender;
-    public void DrawNoiseMap(float[,] noiseMap)
+    public MeshFilter meshFilter;
+    public MeshRenderer meshRenderer;
+
+    public void DrawTexture(Texture2D texture)
     {
-        
+        textureRender.sharedMaterial.mainTexture = texture;
+        textureRender.transform.localScale = new Vector3(texture.width, 1, texture.height);
+    }
+
+    public void DrawMesh(MeshData meshData, Texture2D texture)
+    {
+        meshFilter.sharedMesh = meshData.CreateMesh();
+        meshRenderer.sharedMaterial.mainTexture = texture;
     }
 }
--- Game/Assets/Scripts/MapGenerator.cs
+++ Game/Assets/Scripts/MapGenerator.cs
@@ -2,15 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapGenerator : MonoBehaviour {
+public class MapGenerator : MonoBehaviour
+{
+    public enum DrawMode
+    {
+        Noisemap,
+        ColourMap,
+        Mesh
+    };
 
-    public int mapWidth;
-    public int mapHeight;
+    private const int mapChunkSize = 241;
+    [Range(0,6)]
+    public int levelOfDetail;
+    public DrawMode drawmode;
+
     public float noiseScale;
+
+    public int octaves;
+    [Range(0, 1)] public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offest;
+
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
+
+    public bool autoUpdate;
+
+    public TerrainType[] regions;
 
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance,
+            lacunarity, offest);
 
+        Color[] colourMap = new Color[mapChunkSize * mapChunkSize];
+        for (int y = 0; y < mapChunkSize; y++)
+        {
+            for (int x = 0; x < mapChunkSize; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < regions.Length; i++)
+                {
+                    if (currentHeight <= regions[i].height)
+                    {
+                        colourMap[y * mapChunkSize + x] = regions[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        if (drawmode == DrawMode.Noisemap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromHeighMap(noiseMap));
+        }
+        else if (drawmode == DrawMode.ColourMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapChunkSize, mapChunkSize));
+        }
+        else if (drawmode == DrawMode.Mesh)
+        {
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve,levelOfDetail),
+                TextureGenerator.TextureFromColourMap(colourMap, mapChunkSize, mapChunkSize));
+        }
     }
+
+    private void OnValidate()
+    {
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
+    }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color colour;
 }
